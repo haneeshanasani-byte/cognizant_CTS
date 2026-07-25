654668706448bd5f34273419315ee3642af8092f
Haneesh Anasani <haneeshanasani@gmail.com>
2026-07-25 21:16:58 +0530

    Delete Deepskilling/Entity Framework Core directory

--- Deepskilling/Entity Framework Core/Lab3-RetailInventory/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-// Your Lab 4/5 logic will go here later.
-// For now, this can be empty or just a simple test line.
-Console.WriteLine("Hello, RetailInventory!");
--- Deepskilling/Entity Framework Core/Lab4-RetailInventory/models/Category.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-
-public class Category
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public List<Product> Products { get; set; }
-}
